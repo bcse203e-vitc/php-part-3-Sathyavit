d60f87b2e5083843d3c119cbe47378a683224ebb
Sathyavit <sathya.m2023@vitstudent.ac.in>
2025-10-17 14:59:13 +0600

    Create q2.php

---
+++ Assignment10/q2.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['user'] == "admin" && $_POST['pass'] == "1234") {
+        $_SESSION['user'] = "admin";
+        header("Location: welcome.php");
+        exit();
+    } else {
+        echo "Invalid credentials.";
+    }
+}
+?>
+<form method="post">
+    Username: <input type="text" name="user"><br>
+    Password: <input type="password" name="pass"><br>
+    <input type="submit" value="Login">
+</form>
